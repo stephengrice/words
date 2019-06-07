63f2dc17b96f8a080abbefe4dec2d0090c70dafb
Stephen Grice <stephengrice@live.com>
2019-06-07 10:18:12 -0400

    Add basis for Verb class

--- test/verb.test.js
+++ test/verb.test.js
@@ -3,10 +3,19 @@ var assert = require('assert');
 const Verb = require('../verb');
 
 describe('Verb', () => {
-	it('is a class', () => {
-		let j = new Verb();
+	it('has Tense enum', () => {
+		assert.equal(Verb.Tense.INFINITIVE, 0);
 	});
-	it('has Form enum', () => {
-		assert.equal(Verb.Form.INFINITIVE, 0);
+	it('throws an error when instantiated with no arguments', () => {
+		assert.throws(() => {
+			new Verb();
+		}, Error);
+	});
+	it('successfully instantiates with 3 arguments', () => {
+		new Verb("abide", "abided", "abided");
+	});
+	it('returns the infinitive when asked', () => {
+		let verb = new Verb('infinitive', 'past_indicative', 'past_participle');
+		assert.equal('infinitive', verb.conjugate(Verb.Tense.INFINITIVE));
 	});
 });
--- verb.js
+++ verb.js
@@ -1,10 +1,23 @@
-const Form = {
+const Tense = {
 	INFINITIVE: 0,
 };
 
 class Verb {
-	static get Form() {
-		return Form;
+	static get Tense() {
+		return Tense;
+	}
+	constructor(infinitive, past_indicative, past_participle) {
+		if (infinitive == undefined || past_indicative == undefined || past_participle == undefined) {
+			throw new Error("One or more arguments to the Verb constructor are undefined.");
+		}
+		this.infinitive = infinitive;
+		this.past_indicative = past_indicative;
+		this.past_participle = past_participle;
+	}
+	conjugate(tense) {
+		if (tense == Tense.INFINITIVE) {
+			return this.infinitive;
+		}
 	}
 }
 
