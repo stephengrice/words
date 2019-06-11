c5239a47ed90c92e4059052cb5bad1027996cbf3
Stephen Grice <stephengrice@live.com>
2019-06-11 09:19:50 -0400

    Refactor sentence and paragraph tests

--- test/paragraph.test.js
+++ test/paragraph.test.js
@@ -2,20 +2,20 @@ const assert = require('assert');
 const Paragraph = require('../src/paragraph');
 
 describe('Paragraph', () => {
+  var paragraph, str;
+  beforeEach(() => {
+    paragraph = new Paragraph();
+    str = paragraph.toString();
+  });
   it('has sentences', () => {
-    let paragraph = new Paragraph();
     assert(paragraph.sentences.length > 0);
   });
   it('includes its sentences in toString', () => {
-    let paragraph = new Paragraph();
-    let str = paragraph.toString().toLowerCase();
     for (var i = 0; i < paragraph.sentences.length; i++) {
-      assert(str.includes(paragraph.sentences[i].toString().toLowerCase()), `${str} does not include ${paragraph.sentences[i]}`);
+      assert(str.toLowerCase().includes(paragraph.sentences[i].toString().toLowerCase()), `${str} does not include ${paragraph.sentences[i]}`);
     }
   });
   it('has spaces between sentences', () => {
-    let paragraph = new Paragraph();
-    let str = paragraph.toString();
     for (var i = 0; i < paragraph.sentences.length - 1; i++) { // Skip the last sentence since it's at the very end
         let sentence = paragraph.sentences[i].toString();
         let idx = str.indexOf(sentence);
--- test/sentence.test.js
+++ test/sentence.test.js
@@ -2,26 +2,24 @@ const assert = require('assert');
 const Sentence = require('../src/sentence');
 
 describe('Sentence', () => {
+  var sentence, str;
+  beforeEach(() => {
+    sentence = new Sentence();
+    str = sentence.toString();
+  });
   it('has words', () => {
-    let sentence = new Sentence();
     assert(sentence.words.length > 0);
   });
   it('includes each of its words in toString', () => {
-    let sentence = new Sentence();
-    let str = sentence.toString().toLowerCase();
     for (var i = 0; i < sentence.words.length; i++) {
-      assert(str.includes(sentence.words[i].toString().toLowerCase()));
+      assert(str.toLowerCase().includes(sentence.words[i].toString().toLowerCase()));
     }
   });
   it('ends with punctuation', () => {
-    let sentence = new Sentence();
-    let str = sentence.toString();
     let lastChar = str[str.length - 1];
     assert(lastChar == '.' || lastChar == '!' || lastChar == '?');
   });
   it('has spaces between words', () => {
-    let sentence = new Sentence();
-    let str = sentence.toString();
     for (var i = 0; i < sentence.words.length - 1; i++) { // Skip the last word since it's at the very end
         let word = sentence.words[i].toString();
         let idx = str.indexOf(word);
